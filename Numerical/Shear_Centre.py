87b065d217ba3713ad7f8163b2f04bb6ad3cf20f
MarnixE <61150122+MarnixE@users.noreply.github.com>
2020-02-20 23:16:29 +0000

    Add files via upload

--- Numerical/Shear_Centre.py
+++ Numerical/Shear_Centre.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 20 14:56:29 2020
-
-@author: Marni
-"""
-
 from math import *
 import matplotlib as plt
 import numpy as np
@@ -28,116 +21,68 @@ dtheta = 1e-5
 A = pi*r**2/2
 x_s = (ca-r)/s1
 
+A_circ = r**2*pi/2
+A_triang = s1*r
 
 def qb1(s):
-    qb1 = -t_skin/Izz*(r/(ca-r)*s**2/2)
+    qb1 = -t_skin/Izz*(r/s1*s**2/2)
     return qb1
 
 def qb2(theta):
-    qb2 = -t_skin/Izz*(r**2*cos(theta))
+    qb2 = -t_skin/Izz*(r**2*cos(theta)) + qb1(s1)
     return qb2
 
 def qb3(s):
-    qb3 = -t_skin/Izz*(r*s-s**2/2)
+    qb3 = -t_skin/Izz*(r*s-s**2/2) + qb1(s1)
     return qb3
 
 def qb4(s):
-    return -t_skin/Izz*(-r*s+s**2/2)
+    return -t_skin/Izz*(-r*s+r/s1*s**2/2) + qb3(2*r) + qb2(-pi/2)
+
+twist_circ = 1/A_circ*( (-1/Izz*r**3*(sin(pi/2) - sin(-pi/2) + qb1(s1)*pi*r)) - (-r*1/Izz*(2*r)**2/6 + r/s1*(2*r)**3/6 + qb1(s1)*2*r))
+
+qs0_circ = twist_circ*(t_skin/(pi*r) + t_spar/(2*r))*2*A_circ   
+
+
+twist_triang = 1/2/A_triang*((1/Izz*(r/s1*s1**3/6 + (-r*(2*r)**2/2 + r/s1*(2*r)**3/6) + (-r*s1**2/2 + r/s1*s1**3/6))) + qb1(s1)*2*r + qb2(-pi/2)*s1 + qb3(2*r)*s1)
+
+qs0_triang = twist_triang*(t_skin/(2*s1) + t_spar/(2*r))*2*A_triang
+
+
+def qs1(s):
+    return qb1(s) + qs0_triang
+
+def qs2(s):
+    return qb2(s) + qs0_circ
+
+def qs3(s):
+    return qb3(s) + qs0_triang + qs0_circ
+
+def qs4(s):
+    return qb4(s) + qs0_triang
+
 
 s = 0
-
-qb1_dist = []
-qb2_dist = []
-qb3_dist = []
-qb4_dist = []
-
-s_tot = []
-while s<=s1:
-    q = 0
-    q += qb1(s)
-    if s >= s1/6:
-        q += s1/6*r/(ca-r)*B
-    if s >= 2*s1/6:
-        q += 2*s1/6*r/(ca-r)*B
-    if s >= 3*s1/6:
-        q += 3*s1/6*r/(ca-r)*B
-    if s >= 4*s1/6:
-        q += 4*s1/6*r/(ca-r)*B
-    if s >= 5*s1/6:
-        q += 5*s1/6*r/(ca-r)*B
-        
-    qb1_dist.append(q)
-    s_tot.append(s)
-    s+= ds
-
-#plt.pyplot.plot(s_tot, qb1_dist)
-
-s = 0
-
-while s<=s2:
-    qb3_dist.append(qb3(s)+qb1(s1))
-    s+= ds
-
-theta = pi/2
-
-theta_s = []
-
-while theta >= -pi/2:
-    qb2_dist.append(qb2(theta)+qb1(s1))
-    theta_s.append(theta)
-    theta += -dtheta    
-
-#plt.pyplot.plot(theta_s,qb2_dist)
-s = 0
-
-s_tot = []
-while s<=s1:
-    qb4_dist.append(qb4(s) + qb2(-pi/2) + qb3(s2))
-    s_tot.append(s)
-    s+=ds
-
-plt.pyplot.plot(s_tot, qb4_dist)
-
-#####
-qs01 =  sum(qb2_dist)/len(qb2_dist)
-
-qs02 = sum(qb1_dist)/len(qb2_dist)*s1 + sum(qb4_dist)/len(qb4_dist)*s1 +  (sum(qb4_dist)/len(qb4_dist) - qs01)*2*r
-
-qs1 = []
-qs4 = []
-qs2 = []
-
 n = 0
 ksi = 0
 
-for i in qb1_dist:
-    n +=1
-    qs1.append(i+qs02)
-    ksi += ((ca-r)/s1 * x_s*n)*(i+qs02)/len(qb1_dist)
-    
+while s<=s1:
+    ksi += qs1(s)*ds*(ca-r-x_s*n*ds)
+    n += 1
+    s += ds
+
+s = 0
 n = 0
 
-for i in qb4_dist:
-    n+=1
-    qs1.append(i+qs02)
-    ksi += ((ca-r)/s1 * x_s*n)*(i+qs02)/len(qb1_dist)
-    
+while s<=s1:
+    ksi += qs4(s)*ds*x_s*n*ds
+    n += 1
+    s += ds
+
+theta = pi/2
 n = 0
 
-for i in qb2_dist:
-    n+=1
-    qs2.append(i+qs01)
-    ksi += -(cos(n*dtheta))*(i+qs01)/len(qb2_dist)
-    
-    
-
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
+while theta>=-pi/2:
+    ksi += qs2(theta)*dtheta*cos(theta)
+    n += 1
+    theta += -dtheta
