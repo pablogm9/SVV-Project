0a752a842c2ccf70f4808a7ecb648797fbe8140f
Luke Chin A Foeng <lukegtc@aol.com>
2020-02-20 14:16:37 +0100

    Cubic and quartic integrator

--- Numerical/Numerical_Integration.py
+++ Numerical/Numerical_Integration.py
@@ -14,16 +14,25 @@ OUTPUTS: The sum of the integrated portions for the entire length of the span or
 class Analytical_Int_1D:
     def __init__(self,matrix,coords):
         self.matrix = matrix
-        self.a = [arr[0] for arr in matrix]
-        self.b = [arr[1] for arr in matrix]
-        self.c = [arr[2] for arr in matrix]
-        self.d = [arr[3] for arr in matrix]
+        if len(matrix[0]) == 4:
+            self.a = [arr[0] for arr in matrix]
+            self.b = [arr[1] for arr in matrix]
+            self.c = [arr[2] for arr in matrix]
+            self.d = [arr[3] for arr in matrix]
+        if len(matrix[0]) == 5:
+            self.a = [arr[0] for arr in matrix]
+            self.b = [arr[1] for arr in matrix]
+            self.c = [arr[2] for arr in matrix]
+            self.d = [arr[3] for arr in matrix]
+            self.e = [arr[4] for arr in matrix]
         self.coords = coords
 
     #x_0 and x_1 are the two x values on the chord between which you are integrating
     def integrator(self):
         x_1 = (self.coords[1:])
         x_0 = (self.coords[:-1])
-
-        return sum((self.a*(x_1-x_0)**4)/3 + (self.b*(x_1-x_0)**3)/2 + self.c*(x_1-x_0)**2 + self.d*(x_1-x_0))
+        if len(self.matrix[0]) == 4:
+            return sum((self.a*(x_1-x_0)**4)/3 + (self.b*(x_1-x_0)**3)/2 + self.c*(x_1-x_0)**2 + self.d*(x_1-x_0))
+        if len(self.matrix[0]) == 5:
+            return sum((self.a*(x_1-x_0)**5)/4 + (self.b*(x_1-x_0)**4)/3 + (self.c*(x_1-x_0)**3)/2 + self.d*(x_1-x_0)**2 + self.e(x_1-x_0))
 
