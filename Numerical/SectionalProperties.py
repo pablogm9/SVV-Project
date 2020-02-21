9746028dbe8b846f8a89d16a90cdebfcb97c815a
edgars133 <61150170+edgars133@users.noreply.github.com>
2020-02-21 11:00:03 +0100

    Update SectionalProperties.py

--- Numerical/SectionalProperties.py
+++ Numerical/SectionalProperties.py
@@ -119,7 +119,21 @@ def get_MoI(ha,ca,t_st,w_st,h_st,A_st,t_sk,t_sp,nstiff)
     
     return Izz,Iyy
 
-
+def get_J(G,ha,t_sk,w_st,t_sp,t_st,Ca):
+    A1=(ha/2)**2*pi/2
+    A2=sqrt((ha/2)**2 + (ca-ha/2)**2)*ha/2
+    randtorque=1
+    Y=[randtorque,
+    0]
+    AA=np.matrix([[2*A1, 2*A2],
+                 [1/(2*A1*G)*((pi*ha/2 - 3*w_st)/t_sk + 3*w_st/(t_sk+t_st)) + ha/(2*A2*G*t_sp) + ha/(2*A1*G*t_sp), - 1/(2*A2*G)*((2*sqrt((ha/2)**2+(c-ha/2)**2) - 8*w_st)/t_sk + 8*w_st/(t_sk+t_st)) - ha/(2*A2*G*t_sp) - ha/(2*A1*G*t_sp)]])
+    shears=np.linalg.solve(AA,Y)
+    q1=shears[0]
+    q2=shears[1]
+    dodz =  1/(2*A1)*(q1/G*((pi*ha/2 - 3*w_st)/t_sk + 3*w_st/(t_sk+t_st)) + (q1-q2)/G*(ha)/t_sp)
+    dodz2=1/(2*A2)*(q2/G*((2*sqrt((ha/2)**2+(c-ha/2)**2) - 8*w_st)/t_sk + 8*w_st/(t_sk+t_st)) + (q2-q1)/G*(ha/t_sp))
+    J=randtorque/(G*dodz)
+    return J
 
 
 def get_SC(t_st,w_st,h_st,t_sk,t_sp,V,ha,Ca,Izz,):
