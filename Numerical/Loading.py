fc809b47bdbc99035c88c632a3353ba6d9dc6b5b
edgars133 <61150170+edgars133@users.noreply.github.com>
2020-02-28 14:08:24 +0100

    Add files via upload

--- Numerical/Loading.py
+++ Numerical/Loading.py
@@ -80,8 +80,8 @@ def matrix_solver(c,J,L,x1,x2,x3,xa,theta,t_st,t_sk,t_sp,w_st,ha,Py,Pz,d1,d2,d3,
        0,  #- DTd2/(G*J)*(0) - (0-zshear+ha/2)/(G*J)*(0)*Py*Macauley(x2-(x2+xa/2))**1 + ha/(2*G*J)*0*Pz*Macauley(x2-(x2+xa/2))**1,                                                                        #w(x2) = 0
        -d3*sin(theta)+Pz/(E*Iyy)*1/6*Macauley(x3-(x2+xa/2))**3, #- DTd3/(G*J)*(0) - (0-zshear+ha/2)/(G*J)*(0)*Py*Macauley(x3-(x2+xa/2))**1 + ha/(2*G*J)*0*Pz*Macauley(x3-(x2+xa/2))**1,                                                         #w(x3) = -d3*sin(theta)
        #Pz/(E*Iyy)*1/6*Macauley(x2-xa/2-(x2+xa/2))**3 - DTd4/(G*J)*(ha/2) - (0-zshear)/(G*J)*(ha/2)*Py*Macauley(x2-xa/2-(x2+xa/2))**1 + ha/(2*G*J)*ha/2*Pz*Macauley(x2-xa/2-(x2+xa/2))**1,                                                    #[w(x2-xa/2)+twist(x2-xa/2)*ha/2]cos(theta) + [v(x2-xa/2) + (0-zshear+ha/2)*twist(x2-xa/2)]*sin(theta)=0                at ACTUATOR 1
-       cos(theta)*(DTd4/(G*J)*(ha/2)) + (-FI4/(Izz*E)  -(0-zshear+ha/2)*DTd4/(G*J))*sin(theta),
-       #cos(theta)*(-DTd4/(G*J)*(ha/2)) + (FI4/(Izz*E)  -(0-zshear+ha/2)*DTd4/(G*J))*sin(theta),
+       #cos(theta)*(-DTd4/(G*J)*(ha/2)) + (-FI4/(Izz*E)  +(0-zshear+ha/2)*DTd4/(G*J))*sin(theta),
+       cos(theta)*(-DTd4/(G*J)*(ha/2)) + (FI4/(Izz*E)  -(0-zshear+ha/2)*DTd4/(G*J))*sin(theta),
        0]          #Ay - Aztan(30) = 0
 #This Y vector includes all boundary conditions - RHS
 #The equation becomes XX*RES = Y
@@ -96,7 +96,8 @@ def matrix_solver(c,J,L,x1,x2,x3,xa,theta,t_st,t_sk,t_sp,w_st,ha,Py,Pz,d1,d2,d3,
                   [0, 0, 0,  1/(E*Iyy)*1/6*(Macauley(x1-x1)**3), 1/(E*Iyy)*1/6*(Macauley(x1-x2)**3), 1/(E*Iyy)*1/6*(Macauley(x1-x3)**3),    0, -1/(E*Iyy)*1/6*Macauley(x1-(x2-xa/2))**3,   0,0,x1,1,0  ],
                   [0, 0, 0,  1/(E*Iyy)*1/6*(Macauley(x2-x1)**3), 1/(E*Iyy)*1/6*(Macauley(x2-x2)**3), 1/(E*Iyy)*1/6*(Macauley(x2-x3)**3),    0, -1/(E*Iyy)*1/6*Macauley(x2-(x2-xa/2))**3,   0,0,x2,1,0  ],
                   [0, 0, 0,  1/(E*Iyy)*1/6*(Macauley(x3-x1)**3), 1/(E*Iyy)*1/6*(Macauley(x3-x2)**3), 1/(E*Iyy)*1/6*(Macauley(x3-x3)**3),    0, -1/(E*Iyy)*1/6*Macauley(x3-(x2-xa/2))**3,   0,0,x3,1,0],
-                  [sin(theta)*(-1/(E*Izz)*1/6*(Macauley((x2-xa/2)-x1)**3)+(0-zshear+ha/2)*1/(G*J)*(0-zshear)*Macaulay(x2-xa/2-x1)**1) +cos(theta)*(-ha/(2*G*J)*(0-zshear)*Macaulay(x2-xa/2-x1)**1), 0, 0,cos(theta)*(1/(E*Iyy)*1/6*(Macauley(x2-xa/2-x1)**3)),0, 0, 0, 0, sin(theta)*(x2-xa/2),sin(theta),cos(theta)*(x2-xa/2),cos(theta),-(0-zshear+ha/2)*sin(theta)+ha/2*cos(theta)],
+                  #+[sin(theta)*(-1/(E*Izz)*1/6*(Macauley((x2-xa/2)-x1)**3)+(0-zshear+ha/2)*1/(G*J)*(0-zshear)*Macaulay(x2-xa/2-x1)**1) +cos(theta)*(-ha/(2*G*J)*(0-zshear)*Macaulay(x2-xa/2-x1)**1), 0, 0,cos(theta)*(1/(E*Iyy)*1/6*(Macauley(x2-xa/2-x1)**3)),0, 0, 0, 0, sin(theta)*(x2-xa/2),sin(theta),cos(theta)*(x2-xa/2),cos(theta),-(0-zshear+ha/2)*sin(theta)+ha/2*cos(theta)],
+                  [sin(theta)*(1/(E*Izz)*1/6*(Macauley((x2-xa/2)-x1)**3)-(0-zshear+ha/2)*1/(G*J)*(0-zshear)*Macaulay(x2-xa/2-x1)**1) +cos(theta)*(-ha/(2*G*J)*(0-zshear)*Macaulay(x2-xa/2-x1)**1), 0, 0,cos(theta)*(-1/(E*Iyy)*1/6*(Macauley(x2-xa/2-x1)**3)),0, 0, 0, 0, -sin(theta)*(x2-xa/2),-sin(theta),cos(theta)*(x2-xa/2),cos(theta),(0-zshear+ha/2)*sin(theta)+ha/2*cos(theta)],
                   [0,0,0, 0,0,0, 1,-tan(theta), 0,0,0,0,0]])
     #RES=[R1y,R2y,R3y,R1z,R2z,R3z,Ay,Az,C1,C2,C3,C4,C5]
     RES=np.linalg.solve(XX,Y)
@@ -113,7 +114,7 @@ def moment_y(x,R1z,R2z,R3z,Az,Pz,x1,x2,x3,xa):
     My = -R1z*Macaulay(x-x1) + Pz*Macaulay(x-(x2+xa/2)) - R2z*Macaulay(x-x2) - R3z*Macaulay(x-x3)+ Az*Macaulay(x-(x2-xa/2))
     return(My)
 def torque_x(x,Td,zshear,R1y,R2y,R3y,Ay,Az,Py,Pz,x1,x2,x3,xa,ha):
-    T = (-Td - (0-zshear)*R1y*Macaulay_power_0(x-x1) - (0-zshear)*R2y*Macaulay_power_0(x-x2) - (0-zshear)*R3y*Macaulay_power_0(x-x3) + (0-zshear+ha/2)*Py*Macaulay_power_0(x-(x2+xa/2)) - (ha/2)*Pz*Macaulay_power_0(x-(x2+xa/2)) - (ha/2)*Az*Macaulay_power_0(x-(x2-xa/2)) + (0-zshear+ha/2)*Ay*Macaulay_power_0(x-(x2-xa/2)))
+    T = -(-Td - (0-zshear)*R1y*Macaulay_power_0(x-x1) - (0-zshear)*R2y*Macaulay_power_0(x-x2) - (0-zshear)*R3y*Macaulay_power_0(x-x3) + (0-zshear+ha/2)*Py*Macaulay_power_0(x-(x2+xa/2)) - (ha/2)*Pz*Macaulay_power_0(x-(x2+xa/2)) - (ha/2)*Az*Macaulay_power_0(x-(x2-xa/2)) + (0-zshear+ha/2)*Ay*Macaulay_power_0(x-(x2-xa/2)))
     return(T)
 def shear_y(x,R1y,R2y,R3y,Ay,Py,S,x1,x2,x3,xa):
     Sy = -S + R1y*Macaulay_power_0(x-x1) + R2y*Macaulay_power_0(x-x2) - Py*Macaulay_power_0(x-(x2+xa/2)) - Ay*Macaulay_power_0(x-(x2-xa/2)) + R3y*Macaulay_power_0(x-x3)
@@ -122,7 +123,7 @@ def shear_z(x,R1z,R2z,R3z,Az,Pz,x1,x2,x3,xa):
     Sz = -R1z*Macaulay_power_0(x-x1) - R2z*Macaulay_power_0(x-x2) + Pz*Macaulay_power_0(x-(x2+xa/2)) + Az*Macaulay_power_0(x-(x2-xa/2)) - R3z*Macaulay_power_0(x-x3)
     return(Sz)
 def twist(x,G,J,DTd,R1y,R2y,R3y,Py,Pz,Ay,Az,C5,x1,x2,x3,xa,ha,zshear):
-    twist = 1/(G*J)*(DTd - (0-zshear)*R1y*Macaulay(x-x1)**1 - (0-zshear)*R2y*Macaulay(x-x2)**1 - (0-zshear)*R3y*Macaulay(x-x3)**1 + (0-zshear+ha/2)*Py*Macaulay(x-(x2+xa/2))**1 - (ha/2)*Pz*Macaulay(x-(x2+xa/2))**1 - (ha/2)*Az*Macaulay(x-(x2-xa/2))**1 + (0-zshear+ha/2)*Ay*Macaulay(x-(x2-xa/2))**1 ) + C5
+    twist = -(1/(G*J)*(DTd - (0-zshear)*R1y*Macaulay(x-x1)**1 - (0-zshear)*R2y*Macaulay(x-x2)**1 - (0-zshear)*R3y*Macaulay(x-x3)**1 + (0-zshear+ha/2)*Py*Macaulay(x-(x2+xa/2))**1 - (ha/2)*Pz*Macaulay(x-(x2+xa/2))**1 - (ha/2)*Az*Macaulay(x-(x2-xa/2))**1 + (0-zshear+ha/2)*Ay*Macaulay(x-(x2-xa/2))**1 ) + C5)
     return(twist)
     #deflection functions as functions of x in both y and z directions (v in y direction, w in z direction)
 
